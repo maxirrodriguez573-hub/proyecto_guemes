981aafd407e618a219c395c93b9e60e640cc9d64
Maximiliano Rodriguez <maxirrodriguez@gmail.com>
2026-05-15 02:48:41 -0300

    Conexión agregada

--- database/connection.py
+++ database/connection.py
@@ -1,1 +1,18 @@
- 
+import sqlite3 # Importamos sql
+
+def conexion():
+    '''
+    Conexión a la base de datos "proyect", donde este tiene el atributo row_factory para que a la hora de obtener los registros, pueda
+    formatearlos a diccionarios.
+    Este activa las FK cada vez que se conecta a la base de datos.
+    '''
+    conn = sqlite3.connect("C:/Users/maxir/Documents/Proyecto Guemes/proyect/data/proyecto.db")
+
+    conn.row_factory = sqlite3.Row # Para obtener registros en diccionarios
+
+    conn.execute('''PRAGMA foreign_keys = ON''')
+
+    return conn
+
+
+
