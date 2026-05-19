edee8842af624bec1812771a2478f19c2c7786db
Maximiliano Rodriguez <maxirrodriguez@gmail.com>
2026-05-19 00:03:20 -0300

    obtener_comercio, lista_comercios y imports agregados

--- database/comercio.py
+++ database/comercio.py
@@ -1,3 +1,5 @@
+from database.utils import fetchone, fetchall
+
 # Agregar comercio
 def agregar_comercio(conn, nombre : str, direccion : str|None = None, horario : str|None = None) -> int:
     cursor = conn.cursor()
@@ -27,3 +29,32 @@ def eliminar_comercio(conn, comercio_id : int):
     except Exception as E:
         raise Exception("Error al eliminar el comercio") from E
 
+# Obtener un comercio
+def obtener_comercio(conn, comercio_id : int) -> dict:
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute('''
+                       SELECT * FROM COMERCIO 
+                       WHERE comercio_id = (?)
+                       ''', (comercio_id, ))
+        
+        return fetchone(cursor)
+
+    except Exception as E:
+        raise Exception ("Error al obtener la categoria.")from E
+
+# Lista de categorias
+def lista_comercio(conn) -> list[dict]:
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute('''
+                        SELECT * FROM COMERCIO
+                       ''')
+        
+        return fetchall(cursor)
+
+    except Exception as E:
+        raise Exception("Error al obtener la lista de categorias") from E
+ 
