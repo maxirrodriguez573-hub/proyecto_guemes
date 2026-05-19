f0e6dda32fa1584745bc65a969a1187e70eb24b0
Maximiliano Rodriguez <maxirrodriguez@gmail.com>
2026-05-19 00:34:22 -0300

    agregar_gasto y eliminar_gasto agregados

---
+++ database/gasto.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+# Agregar gasto
+def agregar_gasto(conn, telegram_usuario_id : str, categoria_id : int, comercio_id : int, monto : float, recibo_file_id : str, descripcion : str|None = None ,fecha : str|None = None):
+    if not fecha:
+        fecha : str = datetime.now().strftime("%Y-%m-%d %H:%M",)
+
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute('''INSERT INTO GASTO 
+                       (telegram_usuario_id, categoria_id, comercio_id, fecha, monto, descripcion, recibo_file_id)
+                       VALUES ((?), (?), (?), (?), (?), (?), (?))
+                       ''', (telegram_usuario_id, categoria_id, comercio_id, fecha, monto, descripcion, recibo_file_id))
+        
+        conn.commit()
+        return cursor.lastrowid
+
+    except Exception as E:
+        raise Exception ("Error al agregar el gasto.") from E
+    
+# Eliminar gasto
+def eliminar_gasto(conn, gasto_id):
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute('''DELETE FROM GASTO
+                       WHERE gasto_id = (?)''', (gasto_id, ))
+        
+        conn.commit()
+
+    except Exception as E:
+        raise Exception ("Error al eliminar un gasto.") from E
