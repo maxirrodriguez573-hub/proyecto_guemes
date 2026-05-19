4e1d48363e4ce0f32033ff2e3cdbfe1d0530497f
Maximiliano Rodriguez <maxirrodriguez@gmail.com>
2026-05-18 23:42:07 -0300

    fetchall agregado

--- database/utils.py
+++ database/utils.py
@@ -1,3 +1,8 @@
 # Obtener un registro en un diccionario
 def fetchone(cursor) -> dict:
     return dict(cursor.fetchone())
+
+# Obtener lista de registros en una lista de diccionarios
+def fetchall(cursor) -> list[dict]:
+    rows = cursor.fetchall()
+    return [dict(row) for row in rows if row]
