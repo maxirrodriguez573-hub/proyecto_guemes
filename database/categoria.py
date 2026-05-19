87ab5dbc797f0e9a5ac6164ac300e161fcabf48b
Maximiliano Rodriguez <maxirrodriguez@gmail.com>
2026-05-18 23:22:27 -0300

    eliminar_categoria agregada

--- database/categoria.py
+++ database/categoria.py
@@ -13,4 +13,18 @@ def agregar_categoria(conn, nombre : str) -> int:
         
     except Exception as E:
         raise Exception("Error al agregar una categoria.") from E
+
+# Eliminar categoria
+def eliminar_categoria(conn, categoria_id : int):
+    cursor = conn.cursor()
     
+    try:
+        cursor.execute('''DELETE FROM CATEGORIA 
+                       WHERE categoria_id = (?)
+                       ''', (categoria_id, ))
+        
+        conn.commit()
+        
+    except Exception as E:
+        raise Exception("Error al agregar una categoria.") from E
+
