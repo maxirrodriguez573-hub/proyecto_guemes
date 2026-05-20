d53f982e9ccd01bf334d216129ead90f6111f5bc
Maximiliano Rodriguez <maxirrodriguez@gmail.com>
2026-05-20 00:47:22 -0300

    obtener_gasto, lista_gastos agregados + anotaciones + importanciones

--- database/gasto.py
+++ database/gasto.py
@@ -1,7 +1,8 @@
+from database.utils import fetchone, fetchall
 from datetime import datetime
 
 # Agregar gasto
-def agregar_gasto(conn, telegram_usuario_id : str, categoria_id : int, comercio_id : int, monto : float, recibo_file_id : str, descripcion : str|None = None ,fecha : str|None = None):
+def agregar_gasto(conn, telegram_usuario_id : int, categoria_id : int, comercio_id : int, monto : float, recibo_file_id : str, descripcion : str|None = None , fecha : str|None = None) -> int:
     if not fecha:
         fecha : str = datetime.now().strftime("%Y-%m-%d %H:%M",)
 
@@ -20,7 +21,7 @@ def agregar_gasto(conn, telegram_usuario_id : str, categoria_id : int, comercio_
         raise Exception ("Error al agregar el gasto.") from E
     
 # Eliminar gasto
-def eliminar_gasto(conn, gasto_id):
+def eliminar_gasto(conn, gasto_id : int):
     cursor = conn.cursor()
 
     try:
@@ -31,3 +32,29 @@ def eliminar_gasto(conn, gasto_id):
 
     except Exception as E:
         raise Exception ("Error al eliminar un gasto.") from E
+
+# Obtener un gasto 
+def obtener_gasto(conn, gasto_id : int) -> dict:
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute('''SELECT * FROM GASTO 
+                       WHERE gasto_id = (?)
+                       ''', (gasto_id, ))
+        
+        return fetchone(cursor)
+
+    except Exception as E:
+        raise Exception ("Error al obtener un gasto") from E
+    
+# Obtener lista de gastos
+def lista_gastos(conn) -> list[dict]:
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute('''SELECT * FROM GASTO''')
+
+        return fetchall(cursor)
+
+    except Exception as E:
+        raise Exception ("Error al obtener los gastos.") from E
