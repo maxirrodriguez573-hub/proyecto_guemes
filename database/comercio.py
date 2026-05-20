d09b4b839aa3f18274c74f0bb9b73a553ce05218
Maximiliano Rodriguez <maxirrodriguez@gmail.com>
2026-05-20 03:19:41 -0300

    modificar_comercio agregado, correciones + Type hints

--- database/comercio.py
+++ database/comercio.py
@@ -1,7 +1,8 @@
 from database.utils import fetchone, fetchall
+import sqlite3 # Type Hints
 
 # Agregar comercio
-def agregar_comercio(conn, nombre : str, direccion : str|None = None, horario : str|None = None) -> int:
+def agregar_comercio(conn : sqlite3.Connection, nombre : str, direccion : str|None = None, horario : str|None = None) -> int:
     cursor = conn.cursor()
 
     try:
@@ -16,7 +17,7 @@ def agregar_comercio(conn, nombre : str, direccion : str|None = None, horario : 
         raise Exception ("Error al agregar un comercio") from E
     
 # Eliminar comercio 
-def eliminar_comercio(conn, comercio_id : int):
+def eliminar_comercio(conn : sqlite3.Connection, comercio_id : int):
     cursor = conn.cursor()
 
     try:
@@ -33,8 +34,8 @@ def eliminar_comercio(conn, comercio_id : int):
         raise Exception("Error al eliminar el comercio") from E
 
 # Obtener un comercio
-def obtener_comercio(conn, comercio_id : int) -> dict:
-    cursor = conn.cursor()
+def obtener_comercio(conn : sqlite3.Connection, comercio_id : int) -> dict:
+    cursor : sqlite3.Cursor = conn.cursor()
 
     try:
         cursor.execute('''
@@ -42,13 +43,18 @@ def obtener_comercio(conn, comercio_id : int) -> dict:
                        WHERE comercio_id = (?)
                        ''', (comercio_id, ))
         
-        return fetchone(cursor)
+        comercio : dict|None = fetchone(cursor)
 
     except Exception as E:
-        raise Exception ("Error al obtener la categoria.")from E
+        raise Exception ("Error al obtener la comercio.")from E
+
+    if not comercio:
+            raise ValueError("No existe el comercio")
+    
+    return comercio
 
 # Lista de comercios
-def obtener_comercios(conn) -> list[dict]:
+def obtener_comercios(conn : sqlite3.Connection) -> list[dict]:
     cursor = conn.cursor()
 
     try:
@@ -56,8 +62,47 @@ def obtener_comercios(conn) -> list[dict]:
                         SELECT * FROM COMERCIO
                        ''')
         
-        return fetchall(cursor)
+        registros : list[dict] = fetchall(cursor)
+
+        # Validación: Error al obtener una lista vacía
+        if not registros:
+            raise Exception ("Error al obtener el comercio.")
+        
+        return registros
 
     except Exception as E:
-        raise Exception("Error al obtener la lista de categorias") from E
- 
+        raise Exception("Error al obtener la lista de comercios") from E
+
+# Modificar comercio
+def modificar_comercio(conn : sqlite3.Connection, comercio_id : int, nombre : str|None = None, direccion : str|None = None, horario : str|None = None) -> int:
+    cursor = conn.cursor()
+
+    # Validación: Parámetros obligatorios 
+    if nombre is None and direccion is None and horario is None:
+        raise ValueError ("No existen cambios.")
+
+    try:
+        # Registro
+        comercio : dict = obtener_comercio(conn, comercio_id)
+
+        # Validaciones
+        if nombre is None:
+            nombre = comercio["nombre"]
+        
+        if direccion is None:
+            direccion = comercio["direccion"]
+
+        if horario is None:
+            horario = comercio["horario"]
+            
+        cursor.execute('''
+                       UPDATE COMERCIO 
+                       SET nombre = (?), direccion = (?), horario = (?) 
+                       WHERE comercio_id = (?)
+                       ''', (nombre, direccion, horario, comercio_id))
+        
+        conn.commit()
+        return cursor.rowcount
+
+    except sqlite3.Error as E:
+        raise Exception ("Error al modificar el comercio.") from E
