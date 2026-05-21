fb737eda84c2a7425d9c81c45a8a4e1b31964337
Maximiliano Rodriguez <maxirrodriguez@gmail.com>
2026-05-21 02:55:21 -0300

    Correciones, Type Hints y validaciones agregadas

--- database/comercio.py
+++ database/comercio.py
@@ -11,26 +11,26 @@ def agregar_comercio(conn : sqlite3.Connection, nombre : str, direccion : str|No
                     VALUES (?, ?, ?)
                        ''', (nombre, direccion, horario))
         conn.commit()
-        return cursor.lastrowid
     
-    except Exception as E:
+    except sqlite3.Error as E:
         raise Exception ("Error al agregar un comercio") from E
+    
+    return cursor.lastrowid
     
 # Eliminar comercio 
 def eliminar_comercio(conn : sqlite3.Connection, comercio_id : int):
     cursor = conn.cursor()
+    
+    comercio = obtener_comercio(conn, comercio_id) # Validar registro 
 
     try:
         cursor.execute('''DELETE FROM COMERCIO
                        WHERE comercio_id = (?)
                        ''', (comercio_id, ))
         
-        if cursor.rowcount == 0:
-            raise ValueError ("Comercio no encontrado.")
-        
         conn.commit()
 
-    except Exception as E:
+    except sqlite3.Error as E:
         raise Exception("Error al eliminar el comercio") from E
 
 # Obtener un comercio
@@ -45,11 +45,11 @@ def obtener_comercio(conn : sqlite3.Connection, comercio_id : int) -> dict:
         
         comercio : dict|None = fetchone(cursor)
 
-    except Exception as E:
+    except sqlite3.Error as E:
         raise Exception ("Error al obtener la comercio.")from E
 
     if not comercio:
-            raise ValueError("No existe el comercio")
+            raise ValueError("Error: No existe el comercio.")
     
     return comercio
 
@@ -62,39 +62,36 @@ def obtener_comercios(conn : sqlite3.Connection) -> list[dict]:
                         SELECT * FROM COMERCIO
                        ''')
         
-        registros : list[dict] = fetchall(cursor)
+        comercios : list[dict] = fetchall(cursor)
 
-        # Validación: Error al obtener una lista vacía
-        if not registros:
-            raise Exception ("Error al obtener el comercio.")
-        
-        return registros
-
-    except Exception as E:
+    except sqlite3.Error as E:
         raise Exception("Error al obtener la lista de comercios") from E
+    
+    return comercios
 
 # Modificar comercio
-def modificar_comercio(conn : sqlite3.Connection, comercio_id : int, nombre : str|None = None, direccion : str|None = None, horario : str|None = None) -> int:
+def modificar_comercio(conn : sqlite3.Connection, comercio_id : int, nombre : str|None = None, direccion : str|None = None, horario : str|None = None) -> dict:
     cursor = conn.cursor()
 
-    # Validación: Parámetros obligatorios 
-    if nombre is None and direccion is None and horario is None:
+    comercio : dict = obtener_comercio(conn, comercio_id) # Validar registro
+    
+    # Validaciones
+    if nombre is None and direccion is None and horario is None: # Parámetros obligatorios
         raise ValueError ("No existen cambios.")
+    
+    if nombre is None:
+        nombre = comercio["nombre"]
+
+    if not nombre.strip():
+        raise ValueError("Colocar un nombre correcto.")
+
+    if direccion is None:
+        direccion = comercio["direccion"]
+
+    if horario is None:
+        horario = comercio["horario"]
 
     try:
-        # Registro
-        comercio : dict = obtener_comercio(conn, comercio_id)
-
-        # Validaciones
-        if nombre is None:
-            nombre = comercio["nombre"]
-        
-        if direccion is None:
-            direccion = comercio["direccion"]
-
-        if horario is None:
-            horario = comercio["horario"]
-            
         cursor.execute('''
                        UPDATE COMERCIO 
                        SET nombre = (?), direccion = (?), horario = (?) 
@@ -102,7 +99,8 @@ def modificar_comercio(conn : sqlite3.Connection, comercio_id : int, nombre : st
                        ''', (nombre, direccion, horario, comercio_id))
         
         conn.commit()
-        return cursor.rowcount
 
     except sqlite3.Error as E:
         raise Exception ("Error al modificar el comercio.") from E
+    
+    return comercio
