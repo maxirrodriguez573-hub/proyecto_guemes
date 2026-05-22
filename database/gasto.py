1e51d84753871314afbdf7664b3b23b4d304a0c2
Maximiliano Rodriguez <maxirrodriguez@gmail.com>
2026-05-22 00:35:53 -0300

    modificar_gasto(), correciones y Type Hints agregados

--- database/gasto.py
+++ database/gasto.py
@@ -1,12 +1,14 @@
 from database.utils import fetchone, fetchall
 from datetime import datetime
+import sqlite3
 
 # Agregar gasto
-def agregar_gasto(conn, telegram_usuario_id : int, categoria_id : int, comercio_id : int, monto : float, recibo_file_id : str, descripcion : str|None = None , fecha : str|None = None) -> int:
+def agregar_gasto(conn : sqlite3.Connection, telegram_usuario_id : int, categoria_id : int, comercio_id : int, monto : float, recibo_file_id : str, descripcion : str|None = None , fecha : str|None = None) -> int:
+    cursor = conn.cursor()
+
+    # Validaciones 
     if not fecha:
         fecha = datetime.now().strftime("%Y-%m-%d %H:%M")
-
-    cursor = conn.cursor()
 
     try:
         cursor.execute('''INSERT INTO GASTO 
@@ -15,28 +17,29 @@ def agregar_gasto(conn, telegram_usuario_id : int, categoria_id : int, comercio_
                        ''', (telegram_usuario_id, categoria_id, comercio_id, fecha, monto, descripcion, recibo_file_id))
         
         conn.commit()
-        return cursor.lastrowid
-
-    except Exception as E:
+        
+    except sqlite3.Error as E:
         raise Exception ("Error al agregar el gasto.") from E
     
+    return cursor.lastrowid
+    
 # Eliminar gasto
-def eliminar_gasto(conn, gasto_id : int):
+def eliminar_gasto(conn : sqlite3.Connection, gasto_id : int):
     cursor = conn.cursor()
+
+    gasto = obtener_gasto(conn, gasto_id) # Validar registro
 
     try:
         cursor.execute('''DELETE FROM GASTO
                        WHERE gasto_id = (?)''', (gasto_id, ))
-        if cursor.rowcount == 0:
-            raise ValueError ("Registro no encontrado.")
-        
+       
         conn.commit()
 
-    except Exception as E:
+    except sqlite3.Error as E:
         raise Exception ("Error al eliminar un gasto.") from E
 
 # Obtener un gasto 
-def obtener_gasto(conn, gasto_id : int) -> dict:
+def obtener_gasto(conn : sqlite3.Connection, gasto_id : int) -> dict:
     cursor = conn.cursor()
 
     try:
@@ -44,10 +47,15 @@ def obtener_gasto(conn, gasto_id : int) -> dict:
                        WHERE gasto_id = (?)
                        ''', (gasto_id, ))
         
-        return fetchone(cursor)
+        gasto = fetchone(cursor)
 
-    except Exception as E:
+    except sqlite3.Error as E:
         raise Exception ("Error al obtener un gasto") from E
+    
+    if not gasto:
+        raise ValueError ("Error: No existe el gasto.")
+
+    return gasto
     
 # Obtener lista de gastos
 def obtener_gastos(conn) -> list[dict]:
@@ -56,8 +64,49 @@ def obtener_gastos(conn) -> list[dict]:
     try:
         cursor.execute('''SELECT * FROM GASTO''')
 
-        return fetchall(cursor)
-
     except Exception as E:
         raise Exception ("Error al obtener los gastos.") from E
+    
+    return fetchall(cursor)
+    
+# Modificar gasto
+def modificar_gasto(conn : sqlite3.Connection,gasto_id : int , telegram_usuario_id : int|None = None, categoria_id : int|None = None, 
+                    comercio_id : int|None = None, fecha : str|None = None, monto : float|None = None, recibo_file_id : int|None = None, 
+                    descripcion : str|None = None) -> int:
+    cursor = conn.cursor()
+
+    gasto = obtener_gasto(conn, gasto_id) # Validar registro
+
+    # Validaciones 
+    if telegram_usuario_id is None:
+        telegram_usuario_id = gasto["telegram_usuario_id"]
+
+    if categoria_id is None:
+        categoria_id = gasto["categoria_id"]
+    
+    if comercio_id is None:
+        comercio_id = gasto["comercio_id"]
+
+    if fecha is None:
+        fecha = gasto["fecha"]
+
+    if monto is None:
+        monto = gasto["monto"]
+
+    if descripcion is None:
+        descripcion = gasto["descripcion"]
+        
+    if recibo_file_id is None:
+        recibo_file_id = gasto[recibo_file_id]
+
+    try:
+        cursor.execute('''UPDATE GASTO 
+                       SET telegram_usuario_id = (?), categoria_id = (?), comercio_id = (?), fecha = (?),
+                       monto = (?), descripcion = (?), recibo_file_id = (?)
+                       ''', telegram_usuario_id, categoria_id, comercio_id, fecha, monto, descripcion, recibo_file_id)
+
+    except sqlite3.Error as E:
+        raise Exception ("Error al modificar el gasto.") from E
+    
+    return cursor.rowcount
     
