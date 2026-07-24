ea25ebae3bca5e7744d4fb25436e80d5b61b40e7
Maximiliano Rodriguez <maxirrodriguez@gmail.com>
2026-07-24 02:14:49 -0300

    Add tests utilities

--- tests/test_categoria.py
+++ tests/test_categoria.py
@@ -3,9 +3,7 @@ from database.categoria import *
 from database.utils import *
 import pytest
 
-class TestAgregarCategoria():
-
-    def obtener_total_categorias(self, conn : sqlite3.Connection) -> int:
+def obtener_total_categorias(conn : sqlite3.Connection) -> int:
         cursor = conn.cursor()
 
         cursor.execute("""SELECT COUNT(*) AS total FROM CATEGORIA""")
@@ -14,24 +12,36 @@ class TestAgregarCategoria():
 
         return total_categorias["total"]
 
-    def eliminar_categoria_de_prueba(self, conn : sqlite3.Connection, categoria_id : int):
+def obtener_categoria_por_id(conn : sqlite3.Connection):
+        def obtener(categoria_id : int) -> dict:
+            cursor = conn.cursor()
+
+            cursor.execute('''SELECT * FROM CATEGORIA WHERE categoria_id == (?)''', (categoria_id, ))
+
+            return fetchone(cursor)
+
+        return obtener
+
+def eliminar_categoria_por_id(conn : sqlite3.Connection, categoria_id : int):
         cursor = conn.cursor()
 
         cursor.execute("""DELETE FROM CATEGORIA WHERE categoria_id == (?)""", (categoria_id, ))
         conn.commit()
 
-    def test_deberia_insertar_una_nueva_categoria(self, conn : sqlite3.Connection):
+
+class TestAgregarCategoria():
+
+    def test_deberia_insertar_una_nueva_categoria(conn : sqlite3.Connection):
         
-        categorias_viejas = self.obtener_total_categorias(conn)
+        categorias_viejas = obtener_total_categorias(conn)
 
         categoria_id = agregar_categoria(conn, "categoria_test")
 
-        categorias_nuevas = self.obtener_total_categorias(conn) 
+        categorias_nuevas = obtener_total_categorias(conn) 
 
         try:
             assert categorias_nuevas == categorias_viejas + 1
         finally:
-            self.eliminar_categoria_de_prueba(conn, categoria_id)
+            eliminar_categoria_por_id(conn, categoria_id)
 
-            
-
+         
