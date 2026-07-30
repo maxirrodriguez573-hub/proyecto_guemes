7af70ee2b2b9b3767dcdbf50d899d85ddae1f721
Maximiliano Rodriguez <maxirrodriguez@gmail.com>
2026-07-30 02:23:26 -0300

    Add tests for categoria module

--- tests/test_categoria.py
+++ tests/test_categoria.py
@@ -12,15 +12,13 @@ def obtener_total_categorias(conn : sqlite3.Connection) -> int:
 
         return total_categorias["total"]
 
-def obtener_categoria_por_id(conn : sqlite3.Connection):
-        def obtener(categoria_id : int) -> dict:
-            cursor = conn.cursor()
+def obtener_categoria_por_id(conn : sqlite3.Connection, categoria_id : int) -> dict:
+        cursor = conn.cursor()
 
-            cursor.execute('''SELECT * FROM CATEGORIA WHERE categoria_id == (?)''', (categoria_id, ))
+        cursor.execute('''SELECT * FROM CATEGORIA WHERE categoria_id == (?)''', (categoria_id, ))
 
-            return fetchone(cursor)
+        return fetchone(cursor)
 
-        return obtener
 
 def eliminar_categoria_por_id(conn : sqlite3.Connection, categoria_id : int):
         cursor = conn.cursor()
@@ -28,10 +26,31 @@ def eliminar_categoria_por_id(conn : sqlite3.Connection, categoria_id : int):
         cursor.execute("""DELETE FROM CATEGORIA WHERE categoria_id == (?)""", (categoria_id, ))
         conn.commit()
 
+@pytest.fixture
+def crear_categoria(conn : sqlite3.Connection) -> callable:
+    categorias_id = []
+
+    def agregar(nombre : str = "categoria_test") -> int:
+        cursor = conn.cursor()
+
+        cursor.execute('''INSERT INTO CATEGORIA (nombre)
+                        VALUES (?)''', (nombre, ))
+        conn.commit()
+
+        categorias_id.append(cursor.lastrowid)
+
+        return cursor.lastrowid
+    
+    yield agregar
+
+    for categoria_id in categorias_id:
+        eliminar_categoria_por_id(conn, categoria_id)
+
+
 
 class TestAgregarCategoria():
 
-    def test_deberia_insertar_una_nueva_categoria(conn : sqlite3.Connection):
+    def test_deberia_insertar_una_nueva_categoria(self, conn : sqlite3.Connection):
         
         categorias_viejas = obtener_total_categorias(conn)
 
@@ -44,4 +63,52 @@ class TestAgregarCategoria():
         finally:
             eliminar_categoria_por_id(conn, categoria_id)
 
-         
+    def test_deberia_insertar_los_datos_correctamente(self, conn : sqlite3.Connection):
+        categoria_id = agregar_categoria(conn, "categoria_test")
+
+        categoria = obtener_categoria_por_id(conn, categoria_id)
+
+        try: 
+            assert categoria["nombre"] == "categoria_test"
+        finally:
+             eliminar_categoria_por_id(conn, categoria_id)
+
+
+
+    @pytest.mark.parametrize("nombres_invalidos",[[], {}, 0, 1, 10.5, False, True, None])
+    def test_deberia_rechazar_tipos_de_datos_invalidos(self, conn : sqlite3.Connection, nombres_invalidos : object):
+        categorias_viejas = obtener_total_categorias(conn)
+
+        with pytest.raises(ValueError):
+            agregar_categoria(conn, nombres_invalidos)
+
+        categorias_nuevas = obtener_total_categorias(conn)
+
+        assert categorias_nuevas == categorias_viejas
+
+    @pytest.mark.parametrize("nombres_vacios", ["", " ", "   ", "\t", "\n", "\t \n"])
+    def test_deberia_rechazar_nombres_vacios(self, conn : sqlite3.Connection , nombres_vacios : str):
+        categorias_viejas = obtener_total_categorias(conn)
+
+        with pytest.raises(ValueError):
+            agregar_categoria(conn, nombres_vacios)
+
+        categorias_nuevas = obtener_total_categorias(conn)
+
+        assert categorias_nuevas == categorias_viejas
+
+    def test_deberia_lanzar_error_si_la_categoria_ya_existe(self, conn : sqlite3.Connection, crear_categoria):
+
+        crear_categoria("categoria_existente")
+
+        with pytest.raises(Exception):
+            agregar_categoria(conn, "categoria_existente")
+
+
+
+
+
+
+
+
+        
